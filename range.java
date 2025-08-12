07af89a20880f56bf07a3a675b93de7304aa1fe1
Axun90-x <howdude9040@gmail.com>
2025-08-12 14:50:28 +0530

    Add files via upload

--- range.java
+++ range.java
@@ -1,10 +1,34 @@
 package main;
 public class range{
     public static void main(String[] args) { 
-        int min=Integer.MIN_VALUE;
-        int max=Integer.MAX_VALUE;
-        System.out.println(min);
-        System.out.println(max);
-        
+            int min=Integer.MIN_VALUE;
+            int max=Integer.MAX_VALUE;
+            System.out.println(min);
+            System.out.println(max);
+
+            double min1=Double.MIN_VALUE;
+            double max1=Double.MAX_VALUE;
+            System.out.println(min1);
+            System.out.println(max1);
+
+            float min2=Float.MIN_VALUE;
+            float max2=Float.MAX_VALUE;
+            System.out.println(min2);
+            System.out.println(max2);
+
+            long min3=Long.MIN_VALUE;
+            long max3=Long.MAX_VALUE;
+            System.out.println(min3);
+            System.out.println(max3);
+
+            short min4=Short.MIN_VALUE;
+            short max4=Short.MAX_VALUE;
+            System.out.println(min4);
+            System.out.println(max4);
+
+            byte min5=Byte.MIN_VALUE;
+            byte max5=Byte.MAX_VALUE;
+            System.out.println(min5);
+            System.out.println(max5);
     }
 }
